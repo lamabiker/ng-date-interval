9a226bfa665d447428596648ba708d97951cc5c2
lamabiker <liam@lamabiker.com>
2020-03-11 17:39:47 +0300

    feat(docs): demo site with examples

--- projects/ng-i18n-date-interval-showcase/src/app/app.component.ts
+++ projects/ng-i18n-date-interval-showcase/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+const datesStr = `dates = {
+  single: new Date(),
+  multiple: ['2001-01-01', '12-12-2002']
+}`;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +12,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-i18n-date-interval-showcase';
-  dates = new Date();
-  otherDates = ['2001-01-01', '12-12-2002'];
+
+  // Test subjects
+  dates = {
+    single: new Date(),
+    multiple: ['2001-01-01', '12-12-2002']
+  };
+
+  // String demos
+  demos = {
+    dates: datesStr,
+    single: '{{dates.single | dateInterval}}',
+    multiple: '{{dates.multiple | dateInterval}}',
+    single_options: "{{dates.single | dateInterval:'mediumDate':'backward'}}",
+    localized: {
+      fr_simple: "{{dates.multiple | dateInterval:'mediumDate':'forward':'fr-FR'}}",
+      ru_simple: "{{dates.multiple | dateInterval:'mediumDate':'forward':'ru-RU'}}",
+      ru_dateFormat: "{{dates.multiple | dateInterval:'dd MMMM y':'forward':'ru-RU'}}"
+    }
+  };
 }
--- projects/ng-i18n-date-interval-showcase/src/app/app.module.ts
+++ projects/ng-i18n-date-interval-showcase/src/app/app.module.ts
@@ -7,14 +7,28 @@ import { AppComponent } from './app.component';
 import localeFr from '@angular/common/locales/fr';
 import localeRu from '@angular/common/locales/ru';
 import { registerLocaleData } from '@angular/common';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 registerLocaleData(localeFr, 'fr-FR');
 registerLocaleData(localeRu, 'ru-RU');
 
 @NgModule({
   declarations: [AppComponent, DateIntervalPipe],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, NoopAnimationsModule, MatCardModule, HighlightModule, MatDividerModule],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        languages: {
+          typescript: () => import('highlight.js/lib/languages/typescript'),
+          xml: () => import('highlight.js/lib/languages/xml')
+        }
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
